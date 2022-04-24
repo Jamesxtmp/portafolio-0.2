4d48e535586324391edfef7df5b833edb699affc
Jamesxtmp <gen22xtmp@gmail.com>
2022-04-24 14:36:21 -0500

    implemented center skills and shadow brightness

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -12,14 +12,20 @@ const Skills = () => {
         const coverage = document.getElementsByClassName('coverage')[0]
         const circle = document.getElementsByClassName('circle')[0]
         clearInterval( interval )
-        element.style.bottom = '20px'
-        element.style.left = '20px'
-        element.style.opacity = '1'
-        element.style.color = `var(--color-${className})`
+        
+        element.style.bottom = '50%'
+        element.style.left = '50%'
+        element.style.fontSize = '300px'
         element.style.zIndex = `6`
+        element.style.color = `var(--color-${className})`
+        setTimeout(()=>{
+            element.style.opacity = '.1'
+            element.style.filter = `blur(2px)`
+        }, 200)
+
         coverage.style.width = '100vh'
         coverage.style.height = '100vh'
-        coverage.style.background = `linear-gradient(67.5deg, var(--colorDark-${className}), var(--color-${className}), var(--colorDark-${className}), var(--bg-secondary))`
+        coverage.style.background = `linear-gradient(to top, var(--colorDark-${className}) , var(--color-${className}) 8%, var(--colorDark-${className})50%, var(--bg-secondary))`
         setTimeout(()=>{
             coverage.style.width = '100vw'
             coverage.style.borderRadius = '0'
@@ -37,8 +43,10 @@ const Skills = () => {
             prevBrightness = randomFix
 
             skillsByClass[ randomFix ].style.opacity = '1'
+            skillsByClass[ randomFix ].style.boxShadow = '0px 0px 100px 100px gray'
             setTimeout( () => {
                 skillsByClass[ randomFix ].style.opacity = ''
+                skillsByClass[ randomFix ].style.boxShadow = ''
             }, 500)
         },1500)
         return () => clearInterval( interval )
